320fc568fd849e724adae75be1a87bc7faefb0b3
Benjamin Piper <68324342+Benjamin-Piper@users.noreply.github.com>
2022-09-25 15:55:52 +1000

    ✨ Added strokes to vehicles

--- Model/Lane.cs
+++ Model/Lane.cs
@@ -16,6 +16,7 @@ namespace Hummer.Model
         private readonly Point origin;
         private readonly List<Vehicle> movingVehicles = new List<Vehicle>();
         private readonly int width;
+        private const int VEHICLE_BORDER_WIDTH = 2;
 
         public Lane(Canvas2DContext context, Direction direction, Point origin, int width)
         {
@@ -29,7 +30,7 @@ namespace Hummer.Model
         {
             foreach (var vehicle in this.movingVehicles)
             {
-                await EraseVehicle(vehicle);
+                await this.EraseVehicle(vehicle);
                 if (this.direction == Direction.Left)
                 {
                     vehicle.X--;
@@ -38,24 +39,65 @@ namespace Hummer.Model
                 {
                     vehicle.X++;
                 }
-                await DrawVehicle(vehicle);
-            } 
+                await this.DrawVehicle(vehicle);
+            }
         }
 
+        /*
+            O = origin (x,y)
+            L = length
+            W = width
+
+            +-------------+
+            |             |
+            |    O---L    |
+            |    |   |    |
+            |    W---+    |
+            |             |
+            +-------------+
+        */
         private async Task DrawVehicle(Vehicle vehicle)
         {
+            // Must be called before every drawing. Not including this cost me hours of debugging :(
+            await this.context.BeginPathAsync();
             await this.context.SetFillStyleAsync(vehicle.Colour);
-            await this.context.FillRectAsync(vehicle.X, GetDrawHeight(vehicle), vehicle.Length, vehicle.Width);
+            await this.context.SetLineWidthAsync(VEHICLE_BORDER_WIDTH);
+            await this.context.RectAsync(
+                vehicle.X,
+                GetDrawHeight(vehicle),
+                vehicle.Length,
+                vehicle.Width
+            );
+            await this.context.FillAsync();
+            await this.context.StrokeAsync();
         }
 
         private double GetDrawHeight(Vehicle vehicle)
         {
-            return this.origin.Y - (vehicle.Width / 2);
+            return this.origin.Y - (vehicle.Width / 2) - VEHICLE_BORDER_WIDTH;
         }
 
+        /*
+            O = origin (x,y)
+            L = length
+            W = width
+
+            O-------------L
+            |             |
+            |    +---+    |
+            |    |   |    |
+            |    +---+    |
+            |             |
+            W-------------+
+        */
         private async Task EraseVehicle(Vehicle vehicle)
         {
-            await this.context.ClearRectAsync(vehicle.X, GetDrawHeight(vehicle), vehicle.Length, vehicle.Width);
+            await this.context.ClearRectAsync(
+                vehicle.X - (VEHICLE_BORDER_WIDTH),
+                GetDrawHeight(vehicle) - (VEHICLE_BORDER_WIDTH),
+                vehicle.Length + (2 * VEHICLE_BORDER_WIDTH),
+                vehicle.Width + (2 * VEHICLE_BORDER_WIDTH)
+            );
         }
 
         public bool IsAvailabe()
