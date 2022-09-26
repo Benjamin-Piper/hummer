f3ba3150407a5decc36a908c71fba10eee4665c1
Benjamin Piper <68324342+Benjamin-Piper@users.noreply.github.com>
2022-09-26 20:46:40 +1000

    🚚 Moved Vehicle under namespace Hummer.Model

--- Model/Vehicle.cs
+++ Model/Vehicle.cs
@@ -1,9 +1,12 @@
-public record class Vehicle
+namespace Hummer.Model
 {
-    public string Colour { get; init; } = "red";
-    public int Length { get; init; }
-    /// <summary>Spawnrate must be between 0 and 1</summary>
-    public double SpawnRate { get; init; }
-    public int Width { get; init; }
-    public int X { get; set; }
+    public record class Vehicle
+    {
+        public string Colour { get; init; } = "red";
+        public int Length { get; init; }
+        /// <summary>Spawnrate must be between 0 and 1</summary>
+        public double SpawnRate { get; init; }
+        public int Width { get; init; }
+        public int X { get; set; }
+    }
 }
