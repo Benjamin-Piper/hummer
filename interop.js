2a0289f9fc54bd977aedbc016c065eba4a470b96
Benjamin-Piper <68324342+Benjamin-Piper@users.noreply.github.com>
2022-10-03 03:18:07 +0000

    Deploying to gh-pages from @ Benjamin-Piper/hummer@8e4a756da1b8cbf6ae9b972eb3094880fa17955c 🚀

---
+++ interop.js
@@ -0,0 +1,42 @@
+const PLAY_NOTHING = 0;
+const VOLUME_OFF = 0;
+const VOLUME_ON = 0.01;
+const gainNodeList = [];
+const oscillatorList = [];
+
+function getWidth() {
+    return window.innerWidth;
+}
+
+function initialiseOscillators(oscillatorCount, frequencies) {
+    const audioContext = new AudioContext();
+
+    for (let i = 0; i < oscillatorCount; i++) {
+        const gainNode = audioContext.createGain();
+        gainNode.gain.value = VOLUME_OFF;
+        gainNodeList.push(gainNode);
+
+        const oscillator = audioContext.createOscillator();
+        /*
+           I'm using sawtooth instead of sine similar to Tania Rascia
+           See https://tania.dev/musical-instrument-web-audio-api/
+           Using sine or triangle requires fades to make it sound nice
+           BUT fades are not supported on firefox
+           See https://bugzilla.mozilla.org/show_bug.cgi?id=1171438
+        */
+        oscillator.type = "sawtooth";
+        oscillator.frequency.value = frequencies[i] ?? PLAY_NOTHING;
+        oscillator.start();
+        oscillatorList.push(oscillator);
+
+        oscillator.connect(gainNode).connect(audioContext.destination);
+    }
+}
+
+function playOscillator(index) {
+    gainNodeList[index].gain.value = VOLUME_ON;
+}
+
+function pauseOscillator(index) {
+    gainNodeList[index].gain.value = VOLUME_OFF;
+}
